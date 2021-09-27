f9cb4081eb35fd5bea16768ca61f23435483bead
jamalaamer <90762240+jamalaamer@users.noreply.github.com>
2021-09-27 10:26:01 +0200

    Add files via upload

---
+++ gauss_pares.py
@@ -0,0 +1,11 @@
+def gauss_pares():
+    n=input("hasta que numero quieres que cuente?")
+    acum=0
+    resto=0
+    for cont in range(1,n+1):
+        resto= cont % 2
+        if(resto==0):
+            acum=acum+cont
+        print(str(cont)+" acum= "+str(acum))
+
+gauss_pares()
