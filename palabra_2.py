a79b7e7c9c66ca3283074729359253d6b80fd5be
jamalaamer <90762240+jamalaamer@users.noreply.github.com>
2021-09-30 08:33:30 +0200

    Add files via upload

---
+++ palabra_2.py
@@ -0,0 +1,8 @@
+def palabra_2():
+    nombre=raw_input("NOMBRE: ")
+    apellido=raw_input("APELLIDO: ")
+    print("Buenos dias, "+nombre+" "+apellido)
+    print("Tu lindo nombre empieza por la letra "+nombre[0])
+
+    
+palabra_2()
