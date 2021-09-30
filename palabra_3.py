fc34d07f05f316df1c364decf842e234370fc284
jamalaamer <90762240+jamalaamer@users.noreply.github.com>
2021-09-30 08:43:45 +0200

    Add files via upload

---
+++ palabra_3.py
@@ -0,0 +1,11 @@
+def palabra_3():
+    nombre=raw_input("NOMBRE: ")
+    apellido=raw_input("APELLIDO: ")
+    print("Buenos dias, "+nombre+" "+apellido)
+    print("Tu lindo nombre empieza por la letra "+nombre)
+    print("Voy a deletrear tu nombre")
+    for cont in range[0,20]
+    print(nombre[0])
+
+    
+palabra_3()
