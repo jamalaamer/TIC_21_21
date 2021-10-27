75e6f441f6af70efbae9afb0f4fa4be0fab95c7d
jamalaamer <90762240+jamalaamer@users.noreply.github.com>
2021-10-27 11:30:18 +0200

    Add files via upload

---
+++ cuentaletras.py
@@ -0,0 +1,17 @@
+
+def cuentaletras():
+    palabra=raw_input("dime una palabra: ")
+    cont=0
+    cont_2=0
+    for letra in palabra:
+        if letra in "aeiou":
+            cont+=1
+    print(str(cont)+ " vocales")
+    for letra in palabra:
+        if letra in"rtypsdfghjklzxcvbnm":
+            cont_2+=1
+    print(str(cont_2)+ " consonantes")
+    
+
+cuentaletras()
+    
---
+++ cuentaletras2.py
@@ -0,0 +1,19 @@
+def cuentaletras2():
+    palabra=raw_input("dime una palabra: ")
+    cont=0
+    cont_2=0
+    for letra in palabra:
+        if letra in "aeiou":
+            cont+=1
+    print(str(cont)+ " vocales")
+    for letra in palabra:
+        if letra in"rtypsdfghjklzxcvbnm":
+            cont+=1
+    print(str(cont)+ " letras")
+    for letra in palabra:
+        if letra in"rtypsdfghjklzxcvbnm":
+            cont_2+=1
+    print(str(cont_2)+ " consonantes")
+    
+
+cuentaletras2()
