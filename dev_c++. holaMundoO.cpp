4a90bac2ebda4dd3f8859bd4fcc569ef4afd1c44
jamalaamer <90762240+jamalaamer@users.noreply.github.com>
2021-12-01 11:59:45 +0100

    Add files via upload

---
+++ dev_c++. holaMundoO.cpp
@@ -0,0 +1,15 @@
+//esto es un comentario
+/*esto es un comentario
+de más líneas*/
+//INSTRUCCIONES AL PREPROCESADOR
+#include<stdio.h>//FICHERO DE CABECERA
+int main(){
+	//DECLARACIÓN DE VARIABLES
+	char salir;
+	printf("Hola mundo cruel");
+	printf("\nPRESIONE CUALQUIER TETCLA PARA SALIR: ");
+	scanf("%c",&salir);
+	return 0;
+
+
+}
