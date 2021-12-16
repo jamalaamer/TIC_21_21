b9d3932f8b31594331fdbb68ea30e4bbf06914cf
jamalaamer <90762240+jamalaamer@users.noreply.github.com>
2021-12-16 08:58:07 +0100

    Add files via upload

---
+++ arrays5.cpp
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main(){
+	char letras[5];
+	int cont;
+	//for cont in range(1,10):
+	for(cont=0;cont<=5;cont++){
+		printf("Introduce la letra %d: ",cont);
+		scanf(" %c",&letras[cont]);
+	}
+	printf("\nHas escrito la palabra: ");
+	for(cont=0;cont<=5;cont++){
+		printf("%c",letras[cont]);
+	}
+	printf("\n");
+	for(cont=4;cont>=0;cont--){
+		printf("%c",letras[cont]);
+	}
+
+	
+	return(0);
+}
+
