09205d273f63fe88a9f258ed89bc1f6756d96a05
jamalaamer <90762240+jamalaamer@users.noreply.github.com>
2022-02-16 11:50:26 +0100

    Add files via upload

---
+++ circulo.java
@@ -0,0 +1,60 @@
+package MiCodigo;
+
+public class circulo {
+	int coordX;
+	int coordY;
+	double radio;
+	/**
+	 * @param coordX
+	 * @param coordY
+	 * @param radio
+	 */
+	public circulo(int coordX, int coordY, double radio) {
+		super();
+		this.coordX = coordX;
+		this.coordY = coordY;
+		this.radio = radio;
+	}
+	/**
+	 * @return the coordX
+	 */
+	public int getCoordX() {
+		return coordX;
+	}
+	/**
+	 * @param coordX the coordX to set
+	 */
+	public void setCoordX(int coordX) {
+		this.coordX = coordX;
+	}
+	/**
+	 * @return the coordY
+	 */
+	public int getCoordY() {
+		return coordY;
+	}
+	/**
+	 * @param coordY the coordY to set
+	 */
+	public void setCoordY(int coordY) {
+		this.coordY = coordY;
+	}
+	/**
+	 * @return the radio
+	 */
+	public double getRadio() {
+		return radio;
+	}
+	/**
+	 * @param radio the radio to set
+	 */
+	public void setRadio(double radio) {
+		this.radio = radio;
+	}
+	public double devuelveArea(){
+		double area;
+		area=(double) (Math.PI*radio*radio);
+		return area;
+	}
+	
+}
